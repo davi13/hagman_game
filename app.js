c17bfadb91a8b29bffccdc8fdfedc1175a68920c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-17 11:33:35 +0200

    adding method that see if a word is include in the original array

---
+++ app.js
@@ -0,0 +1,11 @@
+//Primitive value
+
+//obeject : myObject -->Object.prototype--> null
+//Array: myArray --> Array.prototypes --> Object.prototype --> null
+//Function : myFunction --> Function.prototype --> Object.prototype --> null
+//String : myString --> String.prototype --> Object .prototype --> null
+//Number : myNumber --> Number.prototype --> Object .prototype --> null
+//Boolean : myBoolean --> Boolean.prototype --> Object .prototype --> null
+
+const team = ['Luke', 'Maddisson'];
+console.log(team.hasOwnProperty('filter'));
