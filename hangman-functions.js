e3887025432dcd13a4098c9b94368177331f7da9
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-17 23:54:15 +0200

    added a costum prototype that say the courrent status for the game

--- hangman-functions.js
+++ hangman-functions.js
@@ -5,14 +5,22 @@ const game1 = new Hangman('cat', 3);
 
 puzzleEl.textContent = game1.getPuzzle()
 guessesEl.textContent = game1.remainingGuesses;
-
-
+console.log(game1.status);
 //Listen every key word press from de user
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
     puzzleEl.textContent = game1.getPuzzle();
     guessesEl.textContent = game1.remainingGuesses;
-
-
+    // const myArr = guess.split('');
+    // myArr.forEach(element => {
+    //     if (myArr === game1.guessedLetter) {
+    //         game1.status = 'finish !!!'
+    //         console.log(game1.status);
+    //         console.log(myArr);
+    //     }
+    // });
+    //game1.recalStatus();
+    console.log(game1.status);
 });
+
--- hangman.js
+++ hangman.js
@@ -1,12 +1,33 @@
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
-    this.guessedLetter = [];
+    this.guessedLetters = [];
+    this.status = 'playing';
 }
+
+Hangman.prototype.calculateStatus = function () {
+    let finished = true;
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter)) {
+
+        } else {
+            finished = false;
+        }
+    });
+    if (this.remainingGuesses === 0) {
+        this.status = 'failed!!';
+    } else if (finished) {
+        this.status = 'finished !!!';
+    } else {
+        this.status = 'playing';
+    }
+}
+
 Hangman.prototype.getPuzzle = function () {
     let puzzle = '';
-    this.word.forEach(letter => {
-        if (this.guessedLetter.includes(letter) || letter == ' ') {
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
             puzzle += letter;
         } else {
             puzzle += '*';
@@ -14,19 +35,30 @@ Hangman.prototype.getPuzzle = function () {
     });
     return puzzle;
 }
+// Hangman.prototype.recalStatus = function () {
+//     if (this.guessedLetter.length > 0) {
+//         console.log(this.status);
+//     }
+//     if (this.remainingGuesses <= 0) {
+//         this.status = 'failed !!!';
+//         console.log(this.status);
+//     }
+// }
 
 Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase();
-    const isUnique = !this.guessedLetter.includes(guess);
-    isBadGuess = !this.guessedLetter.includes(guess)
+    const isUnique = !this.guessedLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess)
 
     if (isUnique) {
-        this.guessedLetter.push(guess);
+        this.guessedLetters.push(guess);
     }
     if (isUnique && isBadGuess) {
         this.remainingGuesses--;
     }
+    this.calculateStatus();
 }
+
 
 
 
