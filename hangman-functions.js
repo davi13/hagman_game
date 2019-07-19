fc05a402427ffa71236ac39c399b35e6f571bee8
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-19 11:58:04 +0200

    adding some request to an Api

--- hangman-functions.js
+++ hangman-functions.js
@@ -7,11 +7,22 @@ puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage;
 
 //Listen every key word press from de user
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;
 
 });
+const request = new XMLHttpRequest();
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        console.log(data);
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place');
+    }
+});
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();
 
