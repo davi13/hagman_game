c868e32f2f0f81f2722c3cfa3043aafa184ba325
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-24 12:02:16 +0200

    adding a span for each letter in the game with the css

--- script/hangman-functions.js
+++ script/hangman-functions.js
@@ -14,8 +14,24 @@ window.addEventListener('keypress', (e) => {
 
 });
 const render = () => {
-    puzzleEl.textContent = game1.puzzle;
+    puzzleEl.innerHTML = ''
     guessesEl.textContent = game1.statusMessage;
+    game1.puzzle.split('').forEach((letter) => {
+        const spanEl = document.createElement('span')
+        spanEl.textContent = letter;
+        puzzleEl.appendChild(spanEl);
+
+    })
+
+
+
+
+
+
+    // filteredNotes.forEach((note) => {
+    //     const noteEl = generateNoteDOM(note)
+    //     document.querySelector('#notes').appendChild(noteEl)
+    // })
 
 }
 
